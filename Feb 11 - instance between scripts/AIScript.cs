ed01b4807197eac38807500960ca773280c41d0c
jordmax12 <jordan.max@loop.colum.edu>
2014-02-11 09:58:24 -0600

    feb 11/instance between classes

---
+++ Feb 11 - instance between scripts/AIScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIScript : MonoBehaviour {
+	private PlayerScript GO;
+	public float position;
+	public Vector3 PStempV;
+
+	// Use this for initialization
+	void Start () {
+		GO = GameObject.Find("Player").GetComponent<PlayerScript>();
+	}
+
+	void Awake() {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int stop = 0;
+
+		if (stop == 0)
+		{
+			PStempV = transform.position;
+			/*PStempV.x -= 10;
+			transform.position = (GO.tempV - PStempV);*/
+			PStempV.x = GO._tempV.x - 10;
+			transform.position = PStempV;
+			++stop;
+		}
+
+	}
+
+	void Test() {
+
+
+	}
+}
---
+++ Feb 11 - instance between scripts/PlayerScript.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	
+	// globals
+	public string moveL;
+	public string moveR;
+	public string moveU;
+	public string moveD;
+	//
+	public float playerSpeed;
+	//
+	public Vector3 tempV;
+	public Vector3 _tempV;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tempV = new Vector3 ();
+		tempV = transform.position;
+		_tempV.x = tempV.x;
+		//print ("playerPosition is " + playerPosition);
+		if(Input.GetKey (moveL))
+		{
+			SetTransformX((transform.position.x) - playerSpeed);
+		}
+		if(Input.GetKey (moveR))
+		{
+			SetTransformX((transform.position.x) + playerSpeed);
+		}
+
+		if(Input.GetKey (moveU))
+		{
+			SetTransformY((transform.position.y) + playerSpeed);
+		}
+		if(Input.GetKey (moveD))
+		{
+			SetTransformY((transform.position.y) - playerSpeed);
+		}
+		
+		else
+		{
+			rigidbody.angularVelocity = Vector3.zero;
+		}
+	}
+	
+	void SetTransformX(float n)
+	{
+		transform.position = new Vector3(n, transform.position.y, transform.position.z);
+	}
+
+	void SetTransformY(float n)
+	{
+		transform.position = new Vector3(transform.position.x, n, transform.position.z);
+	}
+}
+//ONLY 50 LINES OF CODE TO MAKE A GAME LIKE FLAPPY BIRD
